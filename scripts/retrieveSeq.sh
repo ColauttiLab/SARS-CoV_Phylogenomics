1e927809cb3ff218fb16e59d3c993fb72553ecd8
robert.colautti@queensu.ca <robert.colautti@queensu.ca>
2020-08-15 11:57:19 -0400

    generic file directory

--- scripts/retrieveSeq.sh
+++ scripts/retrieveSeq.sh
@@ -8,8 +8,10 @@ then
     > "$FILE"
 fi
 
+# NOTE blast directory may vary by user
+
 while read -r CURRENT_LINE
     do
-        /usr/local/ncbi/blast/bin/blastdbcmd -db ~/Dropbox/_Synced/PROJECTS/COVID_Sequencing/QGLO_COVID/inputdata/nextstrain_sequences_051520/nextstrain_sequences_051520.fasta -entry $CURRENT_LINE | tee -a ./seqs.txt
+        /usr/local/ncbi/blast/bin/blastdbcmd -db ./inputdata/nextstrain_sequences_051520/nextstrain_sequences_051520.fasta -entry $CURRENT_LINE | tee -a ./seqs.txt
         ((LINE++))
 done < "outputs/GBACC.txt"
